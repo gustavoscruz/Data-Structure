603e608817563af882605b1d28e7f77d32d0ceeb
gustavoscruz <gustadscruz@gmail.com>
2023-11-24 10:09:07 -0300

    aula 02

--- src/com/gustavo/estruturadados/vetor/Vetor.java
+++ src/com/gustavo/estruturadados/vetor/Vetor.java
@@ -2,10 +2,29 @@ package com.gustavo.estruturadados.vetor;
 
 public class Vetor {
     private String[] elements;
+    private int size;
 
     public Vetor(int capacity){
         this.elements = new String[capacity];
+        this.size = 0;
+    }
 
+    /*public void add (String elements){
+        for(int i = 0; i < this.elements.length; i++){
+            if(this.elements[i] == null){
+                this.elements[i] = elements;
+                break;
+            }
+        }
+    }*/
+
+    public boolean add(String element) {
+        if(this.size < elements.length){
+            elements[this.size] = element;
+            this.size++;
+            return true;
+        }
+        return false;
     }
 
 }
--- src/com/gustavo/estruturadados/vetor/teste/Main.java
+++ src/com/gustavo/estruturadados/vetor/teste/Main.java
@@ -4,9 +4,10 @@ import com.gustavo.estruturadados.vetor.Vetor;
 
 public class Main {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(5);
+        Vetor vetor = new Vetor(2);
 
-        int i = 0;
-        i++;
+        vetor.add("elemento1");
+        vetor.add("elemento2");
+        vetor.add("elemento3");
     }
 }
