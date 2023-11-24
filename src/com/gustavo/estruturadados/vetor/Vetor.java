4071566d07b47d05f8ecd6673e464357e0b225c2
gustavoscruz <gustadscruz@gmail.com>
2023-11-24 09:47:55 -0300

    aula 01

--- src/com/gustavo/estruturadados/vetor/Vetor.java
+++ src/com/gustavo/estruturadados/vetor/Vetor.java
@@ -1,4 +1,11 @@
 package com.gustavo.estruturadados.vetor;
 
 public class Vetor {
+    private String[] elements;
+
+    public Vetor(int capacity){
+        this.elements = new String[capacity];
+
+    }
+
 }
---
+++ src/com/gustavo/estruturadados/vetor/teste/Main.java
@@ -0,0 +1,12 @@
+package com.gustavo.estruturadados.vetor.teste;
+
+import com.gustavo.estruturadados.vetor.Vetor;
+
+public class Main {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+        i++;
+    }
+}
