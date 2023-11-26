14444a67d37b5cf41aa260858f95b0c3cabe2c5a
gustavoscruz <gustadscruz@gmail.com>
2023-11-26 11:16:01 -0300

    aula 10

--- src/com/gustavo/estruturadados/vetor/teste/Main.java
+++ src/com/gustavo/estruturadados/vetor/teste/Main.java
@@ -1,19 +1,14 @@
 package com.gustavo.estruturadados.vetor.teste;
 
 import com.gustavo.estruturadados.vetor.Vetor;
+import com.gustavo.estruturadados.vetor.VetorObject;
 
 public class Main {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(2);
+        VetorObject vetor = new VetorObject(3);
 
-        vetor.add("a");
-        vetor.add("b");
-        vetor.add("c");
-        vetor.add("d");
-
-        System.out.println(vetor);
-
-        vetor.remove(2);
+        vetor.add(3);
+        vetor.add(4);
 
         System.out.println(vetor);
 
