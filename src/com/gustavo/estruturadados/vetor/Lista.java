c854ad7d747e7f8526cbab734f2105895b8ca53f
gustavoscruz <gustadscruz@gmail.com>
2023-11-27 08:02:32 -0300

    Aula 11

---
+++ src/com/gustavo/estruturadados/vetor/Lista.java
@@ -0,0 +1,95 @@
+package com.gustavo.estruturadados.vetor;
+
+public class Lista<T> {
+    private T[] elements;
+    private int size;
+
+    public Lista(int capacity){
+        this.elements = (T[]) new Object[capacity];
+        this.size = 0;
+    }
+
+
+    public boolean add(T element) {
+        increaseCapacity();
+        if(this.size < elements.length){
+            elements[this.size] = element;
+            this.size++;
+            return true;
+        }
+        return false;
+    }
+
+    public void add(int key, T element){
+        if(!(key >= 0 && key < this.size)){
+            throw new IllegalArgumentException("Posição Inválida");
+        }
+
+        increaseCapacity();
+
+        for(int i = size - 1; i >= key; i--){
+            elements[i+1] = elements[i];
+        }
+
+        elements[key] = element;
+        size++;
+    }
+
+    public void remove(int key){
+        if(!(key >= 0 && key < this.size)){
+            throw new IllegalArgumentException("Posição Inválida");
+        }
+        for(int i = key; i < elements.length -1; i++){
+            elements[i] = elements[i + 1];
+        }
+        size--;
+    }
+
+    private void increaseCapacity(){
+        if(size == elements.length){
+            T[] newElements = (T[]) new Object[elements.length * 2];
+            for(int i = 0; i < elements.length; i++){
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public Object search(int key){
+        if(!(key >= 0 && key < this.size)){
+            throw new IllegalArgumentException("Posição Inválida");
+        }
+        return elements[key];
+    }
+
+    public int search(T element){
+        for(int i = 0; i < size; i++){
+            if(elements[i].equals(element)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i = 0; i < this.size - 1; i++) {
+            s.append(this.elements[i]);
+            s.append(", ");
+        }
+        if (this.size > 0) {
+            s.append(this.elements[this.size - 1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
+    }
+}
--- src/com/gustavo/estruturadados/vetor/teste/Main.java
+++ src/com/gustavo/estruturadados/vetor/teste/Main.java
@@ -1,16 +1,13 @@
 package com.gustavo.estruturadados.vetor.teste;
 
-import com.gustavo.estruturadados.vetor.Vetor;
-import com.gustavo.estruturadados.vetor.VetorObject;
+import com.gustavo.estruturadados.vetor.Lista;
 
 public class Main {
     public static void main(String[] args) {
-        VetorObject vetor = new VetorObject(3);
-
-        vetor.add(3);
-        vetor.add(4);
-
-        System.out.println(vetor);
+        Lista<Integer> vetor = new Lista<Integer>(1);
+        vetor.add(2);
+        vetor.add(1);
+        vetor.add(2);
 
     }
 }
